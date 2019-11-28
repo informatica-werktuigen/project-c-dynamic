4d2dd941c507021a537becf33a6f3d7d3afb07b2
Hans Winderix <hans.winderix@skynet.be>
2019-11-28 20:35:06 +0100

    Describe the dynamic memory interface, work in progress

--- memory.c
+++ memory.c
@@ -1,35 +1,80 @@
 #include "memory_priv.h"
 
+/* Initalizes the given list to be the empty list.
+ *
+ * This function is already implemented for you.
+ */
 static void list_init(struct list *list)
 {
   list->first = NULL;
   list->last = NULL;
 }
 
+/* Returns true when the given block is valid.
+ *
+ * This function is already implemented for you.
+ */
+static bool block_is_valid(const struct block *block)
+{
+  return (   (block != NULL)
+          && (block->address >= heap)
+          && (block->address < &(heap[HEAP_SIZE]))
+         );
+}
+
+/* Initalizes the given block with the given address and appends it to the
+ * given list.
+ *
+ * Preconditions:
+ *   - the given list is ordered by ascending address
+ *   - when the list is not empty
+ *     - the given address is greater than the address of the last block in
+ *       the given list:
+ *           block->prev->address < block->address
+ *     - the given address is BLOCK_SIZE bytes greater than the address of the
+ *       last block in the given list:
+ *           block->prev->address + BLOCK_SIZE) == block->address
+ *
+ * Postconditions:
+ *   - the resulting list is ordered by ascending address
+ *   - the last block in the resulting is the given block
+ *   - the block is valid
+ */
 static void list_init_block(struct list  *list,
                             struct block *block,
                             uint8_t      *address)
 {
 }
 
-static bool block_is_valid(const struct block *block)
-{
-  return false;
-}
-
+/* Prints a human representation of the given list in forward order.
+ *
+ * Assuming a value for title of "free list", a list with two elements, with
+ * addresses 0x559cd4da1040 and 0x559cd4da1060 should be printed as follows:
+ *
+ *   free list:
+ *     0x559cd4da1040->0x559cd4da1060->NULL
+ */
 static void list_print(struct list *list, const char *title)
 {
 }
 
+/* Prints a human representation of the given list in reverse order. */
 static void list_print_reverse(struct list *list, const char *title)
 {
 }
 
+/* Returns the length of the given list (the number of blocks it contains) */
 static uint32_t list_get_length(const struct list *list)
 {
   return 0;
 }
 
+/* Returns the block for wich the given address falls within its address
+ * range. The address range of a block starts with its address and ends
+ * with its address + BLOCK_SIZE - 1.
+ *
+ * If the given list does not contain the block, returns NULL.
+ */
 static struct block *list_find_block_by_address(const struct list *list,
                                                 const uint8_t *address)
 {
@@ -64,6 +109,12 @@ static void list_insert_chain(struct list* list, struct block *block)
 {
 }
 
+/* Initializes the dynamic memory and its bookeeping.
+ *
+ * This function is already implemented for you. Study this function
+ * thoroughly in order to understand how the heap and its bookkeeping
+ * is represented.
+ */
 void memory_initialize(void)
 {
   list_init(&free_list);
@@ -78,21 +129,40 @@ void memory_initialize(void)
   assert(address == &(heap[HEAP_SIZE]));
 }
 
+/* Returns the amount of dynamic memory available in number of bytes */
 uint32_t memory_available(void)
 {
   return 0;
 }
 
+/* Returns the amount of dynamic memory used in number of bytes */
 uint32_t memory_used(void)
 {
   return 0;
 }
 
+/* Allocates size number of contiguous bytes and returns a pointer to the
+ * allocated memory. The memory does not have to be initialized.
+ *
+ * Returns NULL,
+ *   if size is zero,
+ *   or if not enough contiguous memory is available
+ */
 void *memory_allocate(uint32_t size)
 {
   return NULL;
 }
 
+/* Releases the memory pointed to by the given pointer, which must have been
+ * returned by a previous call to memory_allocate.
+ *
+ * Returns true when actual memory has been released, false otherwise.
+ *
+ * Possible reasons why the memory has not been released:
+ *  - The given pointer is NULL.
+ *  - The given pointer does not point to memory that was allocated by
+ *    memory_allocate.
+ */
 bool memory_release(void *ptr)
 {
   return false;
